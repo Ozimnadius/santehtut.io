25fc2d2f4f7e9a8563da5e86015f3c4fe3dd5848
Misha-Ser <79251486565@yandex.ru>
2017-04-05 18:59:22 +0400

    адаптив 80%

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(function () {
 
+    $('.hidden-menu__link').on('click',function (e) {
+        e.preventDefault();
+        $('.hidden-menu__list').slideToggle();
+    });
+});
+
+$(function () {
+    $('.menu__title').on('click',function (e) {
+        $('.menu__list').slideToggle();
+    });
+});
