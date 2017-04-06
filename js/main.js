3def4a34d7068f7c09e862d4f3814390266e7f93
Misha-Ser <79251486565@yandex.ru>
2017-04-06 11:49:59 +0400

    На проверку

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $(function () {
 
-    $('.hidden-menu__link').on('click',function (e) {
+    $('.hidden-menu__link').on('click', function (e) {
         e.preventDefault();
         $('.hidden-menu__list').slideToggle();
     });
 });
 
 $(function () {
-    $('.menu__title').on('click',function (e) {
-        $('.menu__list').slideToggle();
-    });
+    if (window.innerWidth < 769) {
+        $('.menu__title').on('click', function (e) {
+            $('.menu__list').slideToggle();
+        });
+    }
 });
